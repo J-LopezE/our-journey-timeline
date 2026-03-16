593546043a326de1a47d5eab0e581bbee2473dbe
J-LopezE <jorgeisrael.lopez@gmail.com>
2026-03-16 14:58:59 -0600

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import ReactGA from 'react-ga4'
 
+const TRACKING_ID = import.meta.env.VITE_GA_ID;
+
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+  
+  ReactGA.send({ 
+    hitType: "pageview", 
+    page: window.location.pathname 
+  });
+} else {
+  console.warn("Google Analytics ID no encontrado. Revisa tu archivo .env o Vercel.");
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
